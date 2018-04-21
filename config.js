37638e63ca4718ad3ea8cfa67470fb0957b4979c
Gustavo Berman <gustavoberman@gmail.com>
2018-04-21 17:54:50 -0300

    cleaning a bit

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 var firebaseconfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+    apiKey: "AIzaSyDw96Wosw205c3kuTaMoPHFpoZZn0Xu94Y",
+    authDomain: "lobot-discord-569ea.firebaseapp.com",
+    databaseURL: "https://lobot-discord-569ea.firebaseio.com",
+    projectId: "lobot-discord-569ea",
+    storageBucket: "lobot-discord-569ea.appspot.com",
+    messagingSenderId: "75353830915"
 };
 
 var config = {
-    token: "",
-    adminID: "",
+    token: "NDM1MDc4NTkwMTQ5Njg5MzQ0.DbTuZQ.LnI8DyZ4hhEMLliAlwpyiDBX7j0",
+    adminID: "9625",
     prefix: "!",
     maxRollsPerDie: 1000,
     descriptorPrepend: "",
     dm: "",
-    swrpg: "",
-    genesys: ""
+    swiaserver: "434766632217477120"
 }
 
   exports.config = config;
   exports.firebaseconfig = firebaseconfig;
+
--- modules/printValues.js
+++ modules/printValues.js
@@ -2,8 +2,8 @@ const config = require("../config.js").config;
 
 function print(str, bot, channelEmoji) {
     let guild = "";
-    if (channelEmoji === "genesys") guild = bot.guilds.get(config.genesys);
-    else guild = bot.guilds.get(config.swrpg);
+    //if (channelEmoji === "genesys") guild = bot.guilds.get(config.genesys);
+    guild = bot.guilds.get(config.swiaserver);
     return guild.emojis.find('name', str).toString();
 }
 
