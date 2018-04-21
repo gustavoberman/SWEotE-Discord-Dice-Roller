53aa703121a610e4a71a73810058412eab108c69
gustavoberman <gustavoberman@gmail.com>
2018-04-21 18:09:51 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+var firebaseconfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+};
+
+var config = {
+    token: "",
+    adminID: "",
+    prefix: "!",
+    maxRollsPerDie: 1000,
+    descriptorPrepend: "",
+    dm: "",
+    swiaserver: ""
+}
+
+  exports.config = config;
+  exports.firebaseconfig = firebaseconfig;
